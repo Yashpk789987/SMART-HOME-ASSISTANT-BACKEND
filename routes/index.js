a9fd239eddd24dab2f259039f0be4cfb1baf0745
Yashpk789987 <43312758+Yashpk789987@users.noreply.github.com>
2018-11-22 15:54:00 +0530

    My first commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+var client = mqtt.connect('mqtt://try:try@broker.shiftr.io', { clientId: 'browser-client' });
+client.on('connect', function(){
+    console.log('client has connected!');
+    client.subscribe('nodemcu/yash');
+    client.subscribe('nodemcu/paras');
+    client.on('message' , (message, topic) => {
+      console.log("i AM RECEIVEING MESSAHGES" , message.toString(), topic.toString());
+    })
+});
+
+router.get('/iot' , (req, res) => {
+  res.render('button')
+})
+
+router.get('/switch' , (req, res) => {
+  client.publish('nodemcu/yash' , req.query.action)
+  res.end();
+})
+
+
+
+module.exports = router;

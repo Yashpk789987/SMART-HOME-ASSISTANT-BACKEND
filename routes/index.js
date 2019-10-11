ed16d6f71ba682fd69ec1b66b71c86f0de720a2a
unknown <Yash Kumar>
2019-10-11 13:17:07 +0530

    commited

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mqtt = require('mqtt');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-// var client = mqtt.connect('mqtt://try:try@broker.shiftr.io', { clientId: 'browser-client' });
-// client.on('connect', function(){
-//     console.log('client has connected!');
-//     client.subscribe('nodemcu/yash');
-//     client.subscribe('nodemcu/paras');
-//     client.on('message' , (message, topic) => {
-//       console.log("i AM RECEIVEING MESSAHGES" , message.toString(), topic.toString());
-//     })
-// });
 
 router.get('/iot', (req, res) => {
   res.render('button');
